61efb6cfbc837bf7a8bc1f970a7f08953731bffa
Clayton Knapp <csjknapp@gmail.com>
2022-04-27 15:14:07 -0500

    error in test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,9 +4,9 @@
 import { screen, render } from '@testing-library/react'
 // 🚨
 // import rest
-import { rest } from 'msw';
+import { rest } from 'msw'
 // import setupServer
-import { setupServer } from 'msw/node';
+import { setupServer } from 'msw/node'
 import App from './App'
 
 //define data here
@@ -14,7 +14,7 @@ const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: 'Vonta',
+  name: 'Sasuke 🌬️🔥',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -24,17 +24,19 @@ const user = {
 
 // 🚨 Create your server
 const server = setupServer(
-  rest.get('https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users?select=*', (req, res, ctx) => res(ctx.json(user)))
-);
+  rest.get('https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users', (req, res, ctx) =>
+    res(ctx.json(user))
+  )
+)
 
 // 🚨 Listen for server start
-beforeAll(() => server.listen());
+beforeAll(() => server.listen())
 
-//optional After each handlers/cleanup
-afterEach(() => server.resetHandlers());
+// optional After each handlers/cleanup
+afterEach(() => server.resetHandlers())
 
 // 🚨 Close server when complete
-afterAll(() => server.close());
+afterAll(() => server.close())
 
 test('Should render the header', async () => {
   render(<App />)
